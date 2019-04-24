7a4f70070fbfcc1a98065a6bf69bedfb2ede1ed3
markvilludo <markanthony.villudo@gmail.com>
2019-04-24 18:29:34 +0800

    updates

--- app/Http/Controllers/WeatherForcastController.php
+++ app/Http/Controllers/WeatherForcastController.php
@@ -15,49 +15,44 @@ class WeatherForcastController extends Controller
     {
 
         $data['cities'] = ['Tokyo', 'Yokohama', 'Kyoto', 'Osaka', 'Sapporo', 'Nagoya'];
-
         return view('index', $data);
 
     }
 
     public function show($city)
     {   
-
         // $apiKey = "04a293d60945a5626768d90e41deea91";
         // https://openweathermap.org/api
-            $url ="api.openweathermap.org/data/2.5/weather?q=Japan,".$city."&appid=04a293d60945a5626768d90e41deea91";
+        $url ="api.openweathermap.org/data/2.5/weather?q=Japan,".$city."&appid=04a293d60945a5626768d90e41deea91";
 
-            $requestContent = [
-               'headers' => [
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json'
-               ]
-            ];
+        $requestContent = [
+           'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+           ]
+        ];
 
-           $client = new \GuzzleHttp\Client();
+       $client = new \GuzzleHttp\Client();
 
-           $apiRequest = $client->request('GET', $url, $requestContent);
+       $apiRequest = $client->request('GET', $url, $requestContent);
 
-           $rawData = json_decode($apiRequest->getBody());
+       $rawData = json_decode($apiRequest->getBody());
 
-           // dd($rawData);
-           // exit;
+        $data = [
+            'weather' => $rawData->weather[0],
+            'main' => $rawData->main,
+            "celsius" => round(($rawData->main->temp - 273.15)),
+            "fahrenheit" => round((($rawData->main->temp - 273.15) * 1.8) + 32),
+            'wind' => $rawData->wind,
+            'rain' => $rawData->rain,
+            'sys' => $rawData->sys,
+            'name' => $rawData->name,
+            'id' => $rawData->id,
+            'date_time' => $rawData->dt,
+            'city' => $city,
+        ];
 
-            $data = [
-                'weather' => $rawData->weather[0],
-                'main' => $rawData->main,
-                "celsius" => round(($rawData->main->temp - 273.15)),
-                "fahrenheit" => round((($rawData->main->temp - 273.15) * 1.8) + 32),
-                'wind' => $rawData->wind,
-                'rain' => $rawData->rain,
-                'sys' => $rawData->sys,
-                'name' => $rawData->name,
-                'id' => $rawData->id,
-                'date_time' => $rawData->dt,
-                'city' => $city,
-            ];
-
-            return view('show_forecast', $data);
+        return view('show_forecast', $data);
     }
 
 }
